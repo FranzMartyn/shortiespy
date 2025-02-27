c121113d5950086791a73f8ba398f908a268bf07
FranzMartyn <fmmartyn10@gmail.com>
2025-02-27 13:43:27 +0100

    init

---
+++ README.md
@@ -0,0 +1,23 @@
+# shortiespy
+
+shortiespy is a simple URL shortener written in python
+
+## Requirements
+
+- Python 3.9 or higher
+- Pip
+- Windows
+
+## Installation
+
+```
+git clone https://github.com/FranzMartyn/shorties
+pip install -r requirements.txt
+```
+
+Start the server with:
+
+```
+python shorties.py
+```
+
---
+++ app.py
@@ -0,0 +1,95 @@
+"""This program contains the routes and starts the server."""
+
+import json  # Everything is stored inside ./data/data.json
+import uuid  # For creating the ids at the end of the url
+import requests  # To check whether the URLs actually exist or not
+
+import flask
+
+
+app = flask.Flask(__name__, static_folder="static", template_folder="static")
+
+JSON_FILE = r"data\data.json"
+ONE_MINUTE = 60
+
+SITES = ["index", "new", "add", "get"]
+
+
+def _is_url_valid(url: str) -> bool:
+    """Returns True if the URL exists."""
+    try:
+        is_ok = requests.get(url, timeout=ONE_MINUTE).ok
+        if not is_ok:
+            return False
+    except requests.exceptions.MissingSchema:
+        return False
+    return True
+
+
+@app.route("/", methods=["GET"])
+def index():
+    """The homepage."""
+    return flask.redirect(flask.url_for("new"))
+
+
+@app.route("/new", methods=["GET"])
+def new():
+    """The site to create a shortened link. The homepage redirects here."""
+    return flask.render_template("index.html")
+
+
+@app.route("/add", methods=["POST"])
+def add():
+    """This checks if the url exists and generates the shorties link"""
+    url = flask.request.form["url"]
+    if not _is_url_valid(url):
+        return flask.render_template("badurl.html",
+                                     link=flask.url_for("new"))
+
+    with open(JSON_FILE, "r", encoding="utf8") as json_file_read:
+        all_urls = json.loads(json_file_read.read())
+
+    urls = list(all_urls.keys())
+    url_id = ""
+    while not url_id or url_id in urls:
+        url_id = str(uuid.uuid4())[0:8]
+
+    all_urls[url_id] = url
+
+    with open(JSON_FILE, "w", encoding="utf8") as json_file_write:
+        json.dump(all_urls, json_file_write)
+
+    OPEN_URL_IN_ANOTHER_TAB = True
+    print(f"{url_id=}")
+    print(f"{all_urls=}")
+    return flask.render_template("successful.html",
+                                 shortie=flask.url_for(
+                                     "get",
+                                     urlid=url_id,
+                                     _external=OPEN_URL_IN_ANOTHER_TAB))
+
+
+@app.route("/g/<urlid>", methods=["GET"])
+def get(urlid):
+    """The endpoint of the shortened links."""
+    with open(JSON_FILE, "r", encoding="utf8") as json_file_read:
+        all_urls = json.loads(json_file_read.read())
+    url = all_urls.get(urlid)
+
+    if not url:
+        return flask.render_template("badshorie.html",
+                                     link=flask.url_for("new"))
+    return flask.redirect(url)
+
+@app.before_request
+def check_if_site_exists():
+    if flask.request.endpoint is None:
+        flask.abort(404)
+
+@app.errorhandler(404)
+def site_not_found():
+    return flask.render_template("badsite.html", flask.url_for(new))
+
+
+if __name__ == "__main__":
+    app.run("localhost", 80, debug=False)
